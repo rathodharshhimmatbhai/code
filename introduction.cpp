ec7d2e4c23bab0b986269590bc0ba02189e7d789
Harsh <rathodharsh428@gmail.com>
2021-06-03 08:38:14 +0530

    added new message in file

--- introduction.cpp
+++ introduction.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n=1;
+    int n=1,sum=0;
     while(n<=10){
         cout<<n<<" ";
+        sum+=n;
         n++;
     }
+    cout<<sum<<endl;
     return 0;
 }
